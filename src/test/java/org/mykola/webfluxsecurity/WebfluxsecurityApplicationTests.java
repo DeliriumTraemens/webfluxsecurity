a596ad17dbd316cfbd5b2cd1281b681c4a44060c
Grigorich <oskinnick@gmail.com>
2023-05-19 14:16:08 +0300

    Initial commit

---
+++ src/test/java/org/mykola/webfluxsecurity/WebfluxsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mykola.webfluxsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
