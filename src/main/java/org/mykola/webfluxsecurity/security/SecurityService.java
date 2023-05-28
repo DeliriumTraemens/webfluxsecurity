a62bf5f30c0ec06ea626e420657f118ef864f431
Grigorich <oskinnick@gmail.com>
2023-05-28 18:07:44 +0300

    SecurityService.java generateToken

--- src/main/java/org/mykola/webfluxsecurity/security/SecurityService.java
+++ src/main/java/org/mykola/webfluxsecurity/security/SecurityService.java
@@ -1,16 +1,48 @@
 package org.mykola.webfluxsecurity.security;
 
+import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import org.mykola.webfluxsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
 public class SecurityService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private Integer expirationInSeconds;
+    @Value("${jwt.issuer}")
+    private String issuer;
+
+    private TokenDetails generateToken(Date expirationDate, Map<String, Object> claims, String subject){
+
+        Date createdDate = new Date();
+        String token = Jwts.builder()
+                    .setClaims(claims)
+                    .setIssuer(issuer)
+                    .setSubject(subject)
+                    .setIssuedAt(createdDate)
+                    .setId(UUID.randomUUID().toString())
+                    .setExpiration(expirationDate)
+                .compact();
+
+        return TokenDetails.builder()
+                    .token(token)
+                    .issuedAt(createdDate)
+                    .expiresAt(expirationDate)
+                .build();
+    }
 
     public Mono<TokenDetails> authenticate(String username, String password){
 
@@ -27,5 +59,5 @@ public class SecurityService {
                 })
                 .switchIfEmpty(Mono.error(new RuntimeException("Username not found")));
     }
-
+// video 34.22
 }
