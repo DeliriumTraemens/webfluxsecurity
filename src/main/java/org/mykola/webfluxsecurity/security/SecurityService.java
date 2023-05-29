ef4b9ba36afc9736c71bc9b38f0f478b9642a03f
Grigorich <oskinnick@gmail.com>
2023-05-29 11:09:36 +0300

    SecurityService.java generateToken overloaded ready

--- src/main/java/org/mykola/webfluxsecurity/security/SecurityService.java
+++ src/main/java/org/mykola/webfluxsecurity/security/SecurityService.java
@@ -3,16 +3,14 @@ package org.mykola.webfluxsecurity.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.mykola.webfluxsecurity.entity.UserEntity;
 import org.mykola.webfluxsecurity.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
-import java.util.Base64;
-import java.util.Date;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @Component
 @RequiredArgsConstructor
@@ -27,44 +25,51 @@ public class SecurityService {
     @Value("${jwt.issuer}")
     private String issuer;
 
-    private TokenDetails generateToken(Map<String, Object> claims, String subject){
-        Long expirationTimeInMillis= expirationInSeconds * 1000L;
-        Date expirationDate= new Date(new Date().getTime() + expirationTimeInMillis);
+    private TokenDetails generateToken(UserEntity user) {
+        Map<String, Object> claims = new HashMap<>() {{
+            put("role", user.getRole());
+        }};
+        return generateToken(claims, user.getId().toString());
+    }
+
+    private TokenDetails generateToken(Map<String, Object> claims, String subject) {
+        Long expirationTimeInMillis = expirationInSeconds * 1000L;
+        Date expirationDate = new Date(new Date().getTime() + expirationTimeInMillis);
 
         return generateToken(expirationDate, claims, subject);
     }
 
-    private TokenDetails generateToken(Date expirationDate, Map<String, Object> claims, String subject){
+    private TokenDetails generateToken(Date expirationDate, Map<String, Object> claims, String subject) {
 
         Date createdDate = new Date();
         String token = Jwts.builder()
-                    .setClaims(claims)
-                    .setIssuer(issuer)
-                    .setSubject(subject)
-                    .setIssuedAt(createdDate)
-                    .setId(UUID.randomUUID().toString())
-                    .setExpiration(expirationDate)
+                .setClaims(claims)
+                .setIssuer(issuer)
+                .setSubject(subject)
+                .setIssuedAt(createdDate)
+                .setId(UUID.randomUUID().toString())
+                .setExpiration(expirationDate)
                 .signWith(SignatureAlgorithm.ES256, Base64.getEncoder().encodeToString(secret.getBytes()))
                 .compact();
 
         return TokenDetails.builder()
-                    .token(token)
-                    .issuedAt(createdDate)
-                    .expiresAt(expirationDate)
+                .token(token)
+                .issuedAt(createdDate)
+                .expiresAt(expirationDate)
                 .build();
     }
 
-    public Mono<TokenDetails> authenticate(String username, String password){
+    public Mono<TokenDetails> authenticate(String username, String password) {
 
 
         return userRepository.findByUserName(username)
-                .flatMap(user ->{
-                        if(!user.isEnabled()){
-                            return Mono.error(new RuntimeException("User is not enabled"));
-                        }
-                        if(!passwordEncoder.matches(password,user.getPassword())){
-                            return Mono.error(new RuntimeException("User password is incorrect"));
-                        }
+                .flatMap(user -> {
+                    if (!user.isEnabled()) {
+                        return Mono.error(new RuntimeException("User is not enabled"));
+                    }
+                    if (!passwordEncoder.matches(password, user.getPassword())) {
+                        return Mono.error(new RuntimeException("User password is incorrect"));
+                    }
                     return Mono.just(new TokenDetails());
                 })
                 .switchIfEmpty(Mono.error(new RuntimeException("Username not found")));
