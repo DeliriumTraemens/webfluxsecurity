b3a552c640bf3ec10efe3dcb255085b7ef1806e9
Grigorich <oskinnick@gmail.com>
2023-05-30 12:54:58 +0300

    Config package and WebSecurityConfig.java added

---
+++ src/main/java/org/mykola/webfluxsecurity/config/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package org.mykola.webfluxsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+
+@Configuration
+@EnableReactiveMethodSecurity
+public class WebSecurityConfig {
+
+}
