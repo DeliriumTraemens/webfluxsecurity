43f33522ecc54d49b306a78729e398de75ef5be6
Grigorich <oskinnick@gmail.com>
2023-05-30 16:16:58 +0300

    RestController and AuthRequest, AuthResonseDto completed

--- src/main/java/org/mykola/webfluxsecurity/rest/AuthRestControllerV1.java
+++ src/main/java/org/mykola/webfluxsecurity/rest/AuthRestControllerV1.java
@@ -2,15 +2,15 @@ package org.mykola.webfluxsecurity.rest;
 
 import lombok.RequiredArgsConstructor;
 import org.mykola.webfluxsecurity.dto.AuthRequestDto;
+import org.mykola.webfluxsecurity.dto.AuthResponseDto;
 import org.mykola.webfluxsecurity.dto.UserDto;
 import org.mykola.webfluxsecurity.entity.UserEntity;
 import org.mykola.webfluxsecurity.mapper.UserMapper;
 import org.mykola.webfluxsecurity.repository.UserRepository;
+import org.mykola.webfluxsecurity.security.CustomPrincipal;
 import org.mykola.webfluxsecurity.security.SecurityService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -28,8 +28,21 @@ public class AuthRestControllerV1 {
     }
 
     @PostMapping("/login")
-    public Mono<AuthRequestDto> login(@RequestBody AuthRequestDto authRequestDto) {
+    public Mono<AuthResponseDto> login(@RequestBody AuthRequestDto dto) {
+        return securityService.authenticate(dto.getUsername(), dto.getPassword())
+                .flatMap(tokenDetails -> Mono.just(AuthResponseDto.builder()
+                        .userId(tokenDetails.getUserId())
+                        .token(tokenDetails.getToken())
+                        .issuedAt(tokenDetails.getIssuedAt())
+                        .expiresAt(tokenDetails.getExpiresAt())
+                        .build()));
+    }
 
+    @GetMapping("/info")
+    public Mono<UserDto>getUserInfo(Authentication authentication){
+        CustomPrincipal customPrincipal = (CustomPrincipal) authentication.getPrincipal();
+        return userRepository.findById(customPrincipal.getId())
+                .map(userMapper::map);
     }
 
 
